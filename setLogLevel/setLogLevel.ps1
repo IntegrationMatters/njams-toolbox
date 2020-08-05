1e749abbf55e54b6e77a8d91d64d7aa555afefe2
sholters <44833404+sholters@users.noreply.github.com>
2020-08-05 17:19:30 +0200

    Update setLogLevel.ps1

--- setLogLevel/setLogLevel.ps1
+++ setLogLevel/setLogLevel.ps1
@@ -2,11 +2,13 @@
 # (c) Integration Matters 2020
 
 param (
-    [string]$instance = "http://localhost:8080/njams",
-    [string]$username = "admin",
-	[string]$password = "admin",
-    [Parameter(Mandatory=$true)] [string][Alias("path")]$domainObjectPath,
-	[Parameter(Mandatory=$true)] [string][Alias("loglevel")]$domainObjectLogLevel # INFO | SUCCESS | WARNING | ERROR
+    [Parameter(Mandatory=$true)][string]$instance,
+    [Parameter(Mandatory=$true)][string]$username = "admin",
+	[Parameter(Mandatory=$true)][string]$password = "admin",
+	[Parameter(Mandatory=$true)][string][Alias("path")]$domainObjectPath,
+	[string][Alias("filter")]$filterProcess = ".*",
+	[Parameter(Mandatory=$true)][string][ValidateSet("INFO", "SUCCESS", "WARNING", "ERROR")][Alias("loglevel")]$domainObjectLogLevel, # INFO | SUCCESS | WARNING | ERROR
+	[switch]$list
 )
 
 # -------------------------------------------------------
@@ -24,20 +26,61 @@ $domainObjectLogLevel = "INFO" # INFO | SUCCESS | WARNING | ERROR
 $myHeader = @{"Authorization" = "Basic"+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes('"$username:$password"'))}
 
 # (1) Login as admin:
-write-output "Login..."
 $myBody = '{ "username": "' + $username + '" , ' + '"password": "' + $password + '" }'
 $mySession = $null #empty variable session
-Invoke-RestMethod -Method POST -Header $myHeader -ContentType "application/json" -Body $myBody -uri "$instance/api/usermanagement/authentication" -SessionVariable mySession
+try {
+	$userId = Invoke-RestMethod -Method POST -Header $myHeader -ContentType "application/json" -Body $myBody -uri "$instance/api/usermanagement/authentication" -SessionVariable mySession
+}
+catch {
+	write-host "Unable to login into nJAMS instance due to:" -ForegroundColor Red
+    write-host "$_.Exception.Message"
+
+	Exit
+}
 
 # (2) Get list of sub elements of given domain object path:
-Write-Output "Get list of domain objects..."
-$domainObjects = Invoke-RestMethod -Method GET -Header $myHeader -ContentType "application/json" -uri "$instance/api/domainobject/path/$domainObjectPath" -WebSession $mySession
+try {
+	$domainObjects = Invoke-RestMethod -Method GET -Header $myHeader -ContentType "application/json" -uri "$instance/api/domainobject/path/$domainObjectPath" -WebSession $mySession
+}
+catch {
+	write-host "Unable to retrieve domain objects from nJAMS instance due to:" -ForegroundColor Red
+    write-host "$_.Exception.Message"
 
-Write-output $domainObjects
+	Exit
+}
 
 # (3) Loop each sub element and set LogLevel:
-$myLogLevel = '{ "logLevel": "' + $domainObjectLogLevel + '" }'
-foreach ($doId in ($domainObjects | Where-Object { $_.name -like "*" }).id){
-	Write-Output "Set LogLevel=$domainObjectLogLevel for domain object Id: $doId"
-	Invoke-RestMethod -Method PUT -Header $myHeader -ContentType "application/json" -Body $myLogLevel -uri "$instance/api/domainobject/$doId" -WebSession $mySession
+try {
+	if (($domainObjects).Count -ne 0) {
+		$counter = 0
+
+		if ($list) {
+			write-output "Domain objects will be just listed, LogLevel settings will remain unchanged:"
+		}
+
+		$myLogLevel = '{ "logLevel": "' + $domainObjectLogLevel + '" }'
+
+		foreach ($do in $domainObjects | Where-Object { $_.name -match $filterProcess -and $_.type -like "*process*"}){
+			$counter++
+			if ($list) {
+				write-output $do
+			}
+			else {
+				Write-Output "Set LogLevel=$domainObjectLogLevel for domain object: $($do.id), $($do.name)"
+				Invoke-RestMethod -Method PUT -Header $myHeader -ContentType "application/json" -Body $myLogLevel -uri "$instance/api/domainobject/$($do.id)" -WebSession $mySession
+			}
+		}
+		if ($counter -eq 0) {
+			write-output "No domain objects of type 'process' found."
+		}
+	}
+	else {
+		write-output "No domain objects found."
+	}
+}
+catch {
+	write-host "Unable to set LogLevel due to:" -ForegroundColor Red
+    write-host "$_.Exception.Message"
+
+	Exit
 }
