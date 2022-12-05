e961af607184bd71c1a32c145915ae8dd7473206
sholters <44833404+sholters@users.noreply.github.com>
2022-12-05 11:40:39 +0100

    Add universal PS script to manage nJAMS Agent

---
+++ njamsAgent/README.md
@@ -0,0 +1,88 @@
+# njams_agent.ps1
+Script to start/stop nJAMS Agent on Linux or Windows machines.
+
+## Description:
+
+nJAMS Agent collects metrics from this machine and sends these metrics to a dedicated nJAMS instance.
+This script starts and stops nJAMS Agent based on the configuration in '<njams-agent-home>/config' by default.
+Before you run this script, nJAMS Agent should be configured. Edit 'njams_agent.conf', respectively 'njams_agent_windows.conf', and configure input and output plugins.
+Please refer to https://docs.integrationmatters.com/projects/agent for more information.
+
+This script runs on Linux and macOS using PowerShell 7 as well as on Windows using Windows PowerShell 5 or PowerShell 7.
+
+
+## How it works:
+
+This script requires an existing installation of nJAMS Agent 4.1 or higher and replaces the existing scripts 'njams_agent.sh' (Linux) and 'start_exe.bat', 'service.cmd', 'uninstall_daemon.cmd' 'start_daemon.cmd', 'stop_daemon.cmd' (Windows) by a universal PowerShell script.
+
+
+## Requirements:
+
+  - nJAMS Agent 4.1 or higher is required
+  - PowerShell is required, see prerequisites below
+  - In case you are using TIBCO EMS input/output plugin, make sure TIBCO EMS libs are available in classpath on this machine.
+
+
+## Setup:
+
+1. Copy 'njams_agent.ps1' into nJAMS Agent's bin folder: '<njams-agent-home>/bin'.
+2. Edit 'njams_agent.conf', respectively 'njams_agent_windows.conf', in '<njams-agent-home>/config' according to your needs
+3. Run this script
+
+```
+SYNTAX
+    ./njams_agent.ps1 [-status]
+                      [-start]
+                      [-stop] 
+                      [-restart]
+                      [-service]
+                      [-install]
+                      [-uninstall]
+                      [<CommonParameters>]
+```
+
+Run `./njams_agent.ps1 -?` to learn more about how to use the script. 
+
+
+## Execution:
+
+* Linux/Unix:
+
+  Run a shell and enter command, for example:
+
+  ```
+  $ pwsh -c './njams_agent.ps1 -start'
+  ```
+
+* Windows:
+
+  Run PowerShell and enter command, for example:
+
+  ```
+  PS C:\> .\njams_agent.ps1 -start
+  ```
+
+* macOS:
+
+  Run a shell and enter command, for example:
+
+  ```
+  $ pwsh -c './njams_agent.ps1 -start'
+  ```
+
+## Prerequisites:
+
+* Linux/Unix: 
+
+  This script requires *PowerShell 7* or higher. Please follow these instructions to install PowerShell 7 on Linux/Unix:
+  https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7
+
+* Windows:
+
+  This script requires *Windows PowerShell 5*, respectively *PowerShell 7* or higher. Please follow these instructions to install PowerShell 7 on Linux/Unix:
+  https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-windows?view=powershell-7
+
+* macOS:
+
+  This script requires *PowerShell 7* or higher. Please follow these instructions to install PowerShell 7 on macOS:
+  https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-macos?view=powershell-7
---
+++ njamsAgent/njams_agent.ps1
@@ -0,0 +1,473 @@
+<#
+.SYNOPSIS
+    Starts / stops nJAMS Agent on this machine.
+
+.DESCRIPTION
+    nJAMS Agent collects metrics from this machine and sends these metrics to a dedicated nJAMS instance.
+    This script starts and stops nJAMS Agent based on the configuration in '<njams-agent-home>/config' by default.
+    Before you run this script, nJAMS Agent should be configured. Edit 'njams_agent.conf', respectively 'njams_agent_windows.conf', and configure input and output plugins.
+    Please refer to https://docs.integrationmatters.com/projects/agent for more information.
+
+    This script runs on Linux and macOS using PowerShell 7 as well as on Windows using Windows PowerShell 5 or PowerShell 7.
+
+    Please note:
+    In case you are using TIBCO EMS output plugin, make sure TIBCO EMS libs are available in classpath on this machine.
+
+.PARAMETER start
+    Starts a process instance of nJAMS Agent. There will be no start, if a running instance of nJAMS Agent is detected.
+    Can be used with additional option '-service' to start nJAMS Agent Windows Service.
+
+.PARAMETER stop
+    Stops the process instance of nJAMS Agent. 
+    Can be used with additional option '-service' to stop nJAMS Agent Windows Service.
+
+.PARAMETER restart
+    Restart process instance of nJAMS Agent. 
+    Can be used with additional option '-service' to restart nJAMS Agent Windows Service.
+
+.PARAMETER status
+    Shows the status of nJAMS Agent, whether process instance is running or stopped.
+    Can be used with additional option '-service' to show the current status of nJAMS Agent Windows Service.
+
+.PARAMETER service
+    This option is used to run nJAMS Agent as Windows Service and can only be used on Windows systems.
+    The following parameters can be used in connection with this option: -start, -restart, -stop, -status, -install, -uninstall.
+
+.PARAMETER install
+    Installs nJAMS Agent as Windows Service. Requires elevated user privilege and can only be used on Windows systems. 
+    
+.PARAMETER uninstall
+    Uninstalls nJAMS Agent Windows Service. Requires elevated user privilege and can only be used on Windows systems.
+
+.EXAMPLE
+    ./njams_agent.ps1 -start
+    Starts an process instance of nJAMS Agent.
+    
+.EXAMPLE
+    ./njams_agent.ps1 -service -start
+    Starts nJAMS Agent Windows Service.
+    
+.EXAMPLE
+    ./njams_agent.ps1 -stop
+    Stops the particular process instance of nJAMS Agent that has been started before with this script.
+
+.EXAMPLE
+    ./njams_agent.ps1 -restart
+    Restarts process instance of nJAMS Agent.
+
+.EXAMPLE
+    ./njams_agent.ps1 -status
+    Shows the running status of this instance of nJAMS Agent.
+	
+.EXAMPLE
+    ./njams_agent.ps1 -service -status
+    Shows the status of nJAMS Agent Windows Service.
+	
+.LINK
+    https://docs.integrationmatters.com/projects/agent
+    https://www.integrationmatters.com/
+
+.NOTES
+    Copyright: (c) 2021 Integration Matters
+#>
+
+param(
+    [switch]$start,
+    [switch]$restart,
+    [switch]$stop,
+    [switch]$status,
+    [switch]$service,
+    [switch]$install,
+    [switch]$uninstall
+)
+
+# Change globals here, if required:
+$agentConfig = "njams_agent.conf"
+$agentBin = "njams_agent"
+$pwshCmd = "pwsh"
+
+# Check runtime env. On PS Core $IsWindows is set automatically.
+if ($PSVersionTable.PSEdition -ne "Core") {
+    $IsWindows = $True
+    $pwshCmd = "powershell"
+}
+
+# For Windows systems set corresponding config for nJAMS Agent:
+if ($IsWindows) {
+    $agentConfig = "njams_agent_windows.conf"
+    $agentBin = "njams_agent.exe"
+}
+
+# For using TIBCO EMS input/output plugin, make sure TIBCO EMS Client libs are installed on this machine and are available in classpath.
+if ($IsWindows) {
+    # Option 1: Add <TIBCO_HOME>\ems\<VERSION>\bin to User Variable 'path':
+    #   $EMS_HOME = "C:\TIBCO\ems\10.1"
+    #   if (($env:path -like "*" + $EMS_HOME + "\bin*") -eq $False) {
+    #       $env:path += ";" + $EMS_HOME + "\bin"
+    #   }
+    # Option 2: Add TIBCO EMS bin path to System Variable 'path'. 
+}
+if ($IsLinux) {
+    # On Linux set $env:LD_LIBRARY_PATH to TIBCO EMS Client libs path.
+    # Option 1: Copy TIBCO EMS Client libs from '<TIBCO_HOME>/ems/<VERSION>/lib' to '<njams-agent-home>/lib' and refer to this location:
+    #   $EMS_LIBS = "$PSScriptRoot/../lib/64:$PSScriptRoot/../lib"
+    # Option 2: Refer to TIBCO EMS Client libs path on this machine:
+    #   $EMS_HOME = "/opt/tibco/ems/8.6"
+    #   $EMS_LIBS = "$EMS_HOME/lib/64:$EMS_HOME/lib"
+    # Set $env_LD_LIBRARY_PATH:
+    #   if (($env:LD_LIBRARY_PATH -like "*" + $EMS_LIBS + "*") -eq $False) {
+    #       $env:LD_LIBRARY_PATH += ":" + $EMS_LIBS
+    #   } 
+}
+
+# Present help, when no paramater is specified:
+if ($PSBoundParameters.Count -eq 0) {
+
+    write-output " "
+    write-output "nJAMS Agent collects metrics from this machine and sends these metrics to a dedicated nJAMS instance."
+    write-output "Type '$PSScriptRoot/njams_agent.ps1 -start' to start nJAMS Agent process instance."
+    write-output "Or type '$PSScriptRoot/njams_agent.ps1 -?' to get more information."
+    write-output " "
+}
+
+# Start nJAMS Agent:
+if ($PSBoundParameters.ContainsKey('start') -eq $True) {
+
+    if ($PSBoundParameters.ContainsKey('service') -eq $True -and $IsWindows) {
+        # Start Windows Service of nJAMS Agent:
+        # Check for existing Windows Service, before starting the service:
+        $serviceObject = Get-Service -name "njams_agent"  -ErrorAction SilentlyContinue
+
+        if ($serviceObject) {
+            if ([string]$serviceObject.Status -eq 'stopped' -and [string]$serviceObject.starttype -ne 'disabled') {
+
+                write-output "Starting nJAMS Agent Windows Service..."
+
+                write-output $(get-date -format "yyyy-MM-dd HH:mm:ss.fff") | out-file $PSScriptRoot/../log/console.log -Append
+                $arguments = "& start-service -name njams_agent -PassThru | Format-List >> $PSScriptRoot/../log/console.log"
+                start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+        
+                write-output "Verify start of service using './njams_agent.ps1 -service -status' or check <njams_agent_home>/log/console.log"
+                write-output " "
+            }
+            else {
+
+                write-output "nJAMS Agent Windows Service is not starting up, current status/starttype is '$($serviceObject.status)/$($serviceObject.starttype)'."
+                write-output " "
+            }
+        }
+        else {
+
+            write-output "nJAMS Agent Windows Service is not installed. Please install nJAMS Agent Windows Service using option '-install' before starting the service."
+            write-output " "
+        }
+    }
+    else {
+        # Start process instance of nJAMS Agent.
+        # Check for existing nJAMS Agent process, before starting up:"
+        $procId = Get-Content $PSScriptRoot/../njams_agent.pid -ErrorAction SilentlyContinue
+
+        if ($procId) {
+
+            $processObject = Get-Process -id $procId -ErrorAction SilentlyContinue
+
+            if ($($processObject.ProcessName) -eq "njams_agent") {
+
+                write-output "nJAMS Agent already running on this machine, pid is $procId"
+                write-output " "
+
+                Exit
+            }
+        }
+
+        write-output "Starting nJAMS Agent..."
+
+        $processObject = Start-Process -NoNewWindow -FilePath "$PSScriptRoot/$agentBin" -PassThru -ArgumentList "--config $PSScriptRoot/../config/$agentConfig" -RedirectStandardError "$PSScriptRoot/../log/console.log"
+
+        if ($processObject) {
+
+            # Save pid into file:
+            write-output $processObject.id > $PSScriptRoot/../njams_agent.pid
+        }
+
+        write-output "nJAMS Agent pid is $($processObject.id)."
+        write-output " "
+    }
+}
+
+# Stop nJAMS Agent:
+if ($PSBoundParameters.ContainsKey('stop') -eq $True) {
+
+    if ($PSBoundParameters.ContainsKey('service') -eq $True -and $IsWindows) {
+        # Stop Windows Service of nJAMS Agent.
+        # Check for existing Windows Service, before stopping the service:
+        $serviceObject = Get-Service -name "njams_agent"  -ErrorAction SilentlyContinue
+
+        if ($serviceObject) {
+            if ([string]$serviceObject.Status -eq 'running') {
+
+                write-output "Stopping nJAMS Agent Windows Service..."
+
+                write-output $(get-date -format "yyyy-MM-dd HH:mm:ss.fff") | out-file $PSScriptRoot/../log/console.log -Append
+                $arguments = "& stop-service -name njams_agent -PassThru | Format-List >> $PSScriptRoot/../log\console.log"
+                start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+            
+                write-output "Verify stop of service using './njams_agent.ps1 -service -status' or check <njams_agent_home>/log/console.log"
+                write-output " "
+            }
+            else {
+
+                write-output "nJAMS Agent Windows Service is not going to stop, current status/starttype is '$($serviceObject.status)/$($serviceObject.starttype)'."
+                write-output " "
+            }
+        }
+        else {
+
+            write-output "nJAMS Agent Windows Service is not installed, no stopping. You can use option '-install' to install nJAMS Agent Windows Service."
+            write-output " "
+        }
+    }
+    else {
+        # Stop process instance of nJAMS Agent.
+        # Check for existing nJAMS Agent process"
+        $procId = Get-Content $PSScriptRoot/../njams_agent.pid -ErrorAction SilentlyContinue
+
+        if ($procId) {
+
+            $processObject = Get-Process -id $procId -ErrorAction SilentlyContinue
+
+            if ($($processObject.ProcessName) -eq "njams_agent") {
+        
+                write-output "Stopping nJAMS Agent at pid $procId ..."
+
+                stop-process -id $procId
+
+                # Wait a bit before before verifying the process is stopped:
+                Start-Sleep -Seconds 2
+
+                # Make sure nJAMS Agent process has terminated successfully:
+                if (get-process -id $procId -ErrorAction SilentlyContinue) {
+        
+                    # Force nJAMS Agent process to stop:
+                    stop-process -id $procId -Force -ErrorAction SilentlyContinue
+                }
+
+                write-output "nJAMS Agent stopped."
+                write-output " "
+        
+                Exit
+            }
+            else {
+
+                write-output "nJAMS Agent process already stopped at pid $procId"
+                write-output " "
+
+                Exit
+            }
+        }
+
+        write-output "nJAMS Agent is not running, no stop required."
+        write-output " "
+    }
+}
+
+# Restart nJAMS Agent process:
+if ($PSBoundParameters.ContainsKey('restart') -eq $True) {
+    if ($PSBoundParameters.ContainsKey('service') -eq $True -and $IsWindows) {
+        # Restart Windows Service of nJAMS Agent.
+        # Check for existing Windows Service, before stopping/starting the service:
+        $serviceObject = Get-Service -name "njams_agent" -ErrorAction SilentlyContinue
+
+        if ($serviceObject) {
+            if ([string]$serviceObject.starttype -ne 'disabled') {
+
+                write-output "Going to restart nJAMS Agent Windows Service now."
+
+                # Stop:
+                if ([string]$serviceObject.Status -ne 'stopped') {
+                    write-output "Stop nJAMS Agent Windows Service..."
+
+                    write-output $(get-date -format "yyyy-MM-dd HH:mm:ss.fff") | out-file $PSScriptRoot/../log/console.log -Append
+                    $arguments = "& stop-service -name njams_agent -PassThru | Format-List >> $PSScriptRoot/../log/console.log"
+                    start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+                }
+
+                # Wait some seconds before starting again:
+                Start-Sleep -Seconds 5
+
+                # Start:
+                $serviceObject = Get-Service -name "njams_agent" -ErrorAction SilentlyContinue
+                if ([string]$serviceObject.Status -eq 'stopped') {
+                    write-output "Start nJAMS Agent Windows Service..."
+
+                    write-output $(get-date -format "yyyy-MM-dd HH:mm:ss.fff") | out-file $PSScriptRoot/../log/console.log -Append
+                    $arguments = "& start-service -name njams_agent -PassThru | Format-List >> $PSScriptRoot/../log/console.log"
+                    start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+
+                    write-output "Verify restart of service using './njams_agent.ps1 -service -status' or check <njams_agent_home>/log/console.log"
+                    write-output " "
+                }
+                else {
+
+                    write-output "Restarting nJAMS Agent Windows Service failed, current status/starttype is '$($serviceObject.status)/$($serviceObject.starttype)'."
+                    write-output " "
+                }
+            }
+            else {
+
+                write-output "nJAMS Agent Windows Service is not going to restart, current status/starttype is '$($serviceObject.status)/$($serviceObject.starttype)'."
+                write-output " "
+            }
+        }
+        else {
+
+            write-output "nJAMS Agent Windows Service is not installed, no restarting. You can use option '-install' to install nJAMS Agent Windows Service."
+            write-output " "
+        }
+    }
+    else {
+
+        write-output "Going to restart nJAMS Agent..."
+
+        # Restart process instance of nJAMS Agent
+        # Check for existing nJAMS Agent process:"
+        $procId = Get-Content $PSScriptRoot/../njams_agent.pid -ErrorAction SilentlyContinue
+
+        if ($procId) {
+
+            $processObject = Get-Process -id $procId -ErrorAction SilentlyContinue
+
+            if ($($processObject.ProcessName) -eq "njams_agent") {
+        
+                write-output "Stopping nJAMS Agent at pid $procId ..."
+
+                stop-process -id $procId
+
+                # Make sure nJAMS Agent process has terminated successfully:
+                if (get-process -id $procId -ErrorAction SilentlyContinue) {
+        
+                    # Force nJAMS Agent process to stop:
+                    stop-process -id $procId -Force
+                }
+
+                write-output "nJAMS Agent stopped."
+                write-output " "
+            }
+        }
+
+        write-output "Starting nJAMS Agent..."
+
+        $processObject = Start-Process -NoNewWindow -FilePath "$PSScriptRoot/$agentBin" -PassThru -ArgumentList "--config $PSScriptRoot/../config/$agentConfig" -RedirectStandardError "$PSScriptRoot/../log/console.log"
+
+        if ($processObject) {
+
+            write-output $processObject.id > $PSScriptRoot/../njams_agent.pid
+        }
+
+        write-output "nJAMS Agent pid is $($processObject.id)."
+        write-output " "
+    }
+}
+
+# Show status of nJAMS Agent:
+if ($PSBoundParameters.ContainsKey('status') -eq $True) {
+
+    if ($PSBoundParameters.ContainsKey('service') -eq $True -and $IsWindows) {
+        # Show status of nJAMS Agent Windows Service
+        # Check for existing Windows Service, before stopping/starting the service:
+        $serviceObject = Get-Service -name "njams_agent" -ErrorAction SilentlyContinue
+
+        if ($serviceObject) {
+
+            Get-Service -name njams_agent | select-object -property status, starttype, name, displayname | Format-List
+        }
+        else {
+
+            write-output "nJAMS Agent Windows Service is not installed. You can use option '-install' to install nJAMS Agent Windows Service."
+            write-output " "
+        }
+    }
+    else {
+        # Check for existing nJAMS Agent process, before starting up:"
+        $procId = Get-Content $PSScriptRoot/../njams_agent.pid -ErrorAction SilentlyContinue
+
+        if ($procId) {
+
+            $processObject = Get-Process -id $procId -ErrorAction SilentlyContinue
+
+            if ($($processObject.ProcessName) -eq "njams_agent") {
+        
+                write-output "nJAMS Agent is running."
+                write-output $processObject | select-object id, name | Format-List
+            }
+            else {
+
+                write-output "nJAMS Agent process stopped at pid $procId"
+                write-output " "
+            }
+
+            Exit
+        }
+        else {
+
+            write-output "nJAMS Agent is not running."
+            write-output " "
+        }
+    }
+}
+# Uninstall nJAMS Agent Windows Service:
+if ($PSBoundParameters.ContainsKey('uninstall') -eq $True -and $IsWindows) {
+
+    # Check, if Windows Service exists:
+    $serviceObject = Get-Service -name "njams_agent" -ErrorAction SilentlyContinue
+
+    if ($serviceObject) {
+
+        if ([string]$serviceObject.Status -eq 'stopped') {
+
+            write-output "Uninstalling nJAMS Agent Windows Service..."
+            
+            $arguments = "& $PSScriptRoot/$agentBin --service uninstall"
+            start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+
+            write-output "nJAMS Agent Windows Service uninstalled."
+            write-output " "
+
+        }
+        else {
+
+            write-output "nJAMS Agent Windows Service is currently running. Please stop service, before uninstalling."
+            write-output "To stop service use './njams_agent.ps1 -service -stop'"
+            write-output " "
+        }
+    }
+    else {
+
+        write-output "nJAMS Agent Windows Service does not exist, no uninstall."
+        write-output " "
+    }
+}
+
+# Install nJAMS Agent Windows Service:
+if ($PSBoundParameters.ContainsKey('install') -eq $True -and $IsWindows) {
+
+    # Check, if Windows Service exists:
+    $serviceObject = Get-Service -name "njams_agent" -ErrorAction SilentlyContinue
+
+    if (!$serviceObject) {
+    
+        write-output "Installing nJAMS Agent Windows Service..."
+        
+        $arguments = "& $PSScriptRoot/$agentBin --service install --config $PSScriptRoot/../config/$agentConfig --config-directory $PSScriptRoot/../config/njams_agent.d"
+        start-process -FilePath $pwshCmd -Verb runAs -ArgumentList "-c", $arguments
+
+        write-output "nJAMS Agent Windows Service installed."
+        write-output " "
+
+    }
+    else {
+
+        write-output "nJAMS Agent Windows Service already exists, no install."
+        write-output " "
+    }
+}
